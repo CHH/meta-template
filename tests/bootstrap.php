08336ff195a93f8fdd35f1d9216db455779eebd2
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-01-07 18:23:56 +0100

    Let composer handle autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,17 +2,3 @@
 
 require_once __DIR__ . "/../vendor/.composer/autoload.php";
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$classLoader = new UniversalClassLoader;
-
-$classLoader->registerNamespace('MetaTemplate', array(
-    realpath(__DIR__ . '/../lib'),
-    realpath(__DIR__)
-));
-
-if (isset($_ENV['MARKDOWN_LIB'])) {
-    require $_ENV['MARKDOWN_LIB'].'/markdown.php';
-}
-
-$classLoader->register();
